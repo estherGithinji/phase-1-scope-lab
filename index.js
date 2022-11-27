a9a3c426b80fa4ccb8566cd26efbea703fd2750e
esther <esther.githinji@student.moringaschool.com>
2022-11-27 23:01:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = 'bob'
+// var bestCustomer = 'not bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+ var setBestCustomer = function(){
+    return bestCustomer = 'not bob'
+ }
+ 
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = 'Jasper'
+
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = 'not Jasper'
+}
+//       it('unsuccessfully tries to reassign the least favorite customer', function() {
+//         expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+//       });
+//     });
+//   });
+  
+ 
